ec331e620b709369eaabdea796c6b1d68119c86f
Kiltonik <valkovmd@mail.ru>
2019-06-05 03:42:38 +0300

    Fixes in working with DB

--- Domain/BDWindowsInteractor.py
+++ Domain/BDWindowsInteractor.py
@@ -17,20 +17,38 @@ class BDWindowsInteractor:
         return BDWindowsInteractor.__instance
 
     def add_entry(self, new_data):
+        number_clear_fields = 0
         for i in range(len(new_data)):
-            if new_data[i] == '':
-                new_data[i] = 'Нет данных'
-        prepared_new_data = {'country': new_data[0],
-                             'province': new_data[1],
-                             'points': new_data[4],
-                             'year': new_data[3],
-                             'taster_name': new_data[6],
-                             'variety': new_data[2],
-                             'price': new_data[5]}
-        self.__BD.add_new_entry(prepared_new_data)
-        self.__MainWindowInteractor.set_new_entry_added_true()
+            if new_data[i] == '\n' or new_data[i] == 'Нет данных' or new_data[i] == '':
+                new_data[i] = None
+                number_clear_fields += 1
+        if number_clear_fields < len(new_data):
+            if new_data[0] not in set(self.__BD.provide_all_countries()):
+                self.__BD.add_new_country(new_data[0])
+            all_provinces = self.__BD.provide_all_provinces()
+            if new_data[1] not in set(all_provinces):
+                self.__BD.add_new_province({'province': new_data[1],
+                                            'id country': list(self.__BD.provide_all_countries()).index(new_data[0])+3})
+            print(self.__BD.provide_all_countries())
+            all_provinces = self.__BD.provide_all_provinces()
+            prepared_new_data = {'province': all_provinces.index(new_data[1]),
+                                     'description': new_data[7],
+                                     'points': new_data[4],
+                                     'year': new_data[3],
+                                     'taster_name': new_data[6],
+                                     'variety': new_data[2],
+                                     'price': new_data[5],
+                                     'title':new_data[8]}
+
+            self.__BD.add_new_wine_entry(prepared_new_data)
+            self.__MainWindowInteractor.set_new_entry_added_true()
 
     def provide_certain_entry(self, entry_id):
-        return dict([j if j[1] == j[1] else (j[0], 'Нет данных') for j in
+        certain_entry = dict([j if j[1] == j[1] else (j[0], 'Нет данных') for j in
                      list(self.__BD.provide_entry_by_id(int(list(entry_id)[0][1:], 16)-1).items())])
-
+        if certain_entry['Province id'] != 'Нет данных':
+            province_data = self.__BD.provide_specific_province(int(certain_entry['Province id']))
+            certain_entry['Province'] = province_data['Province']
+            certain_entry['Country'] = self.__BD.provide_specific_country(
+                province_data['Country id'])
+        return certain_entry
--- Presentation/AddDataWindow.py
+++ Presentation/AddDataWindow.py
@@ -18,72 +18,81 @@ class AddDataWindow(tk.Toplevel):
 
         country_label = tk.Label(self, text='Страна:')
         country_label.grid(row=0, column=0, sticky='w', padx=3, pady=3)
-        country_entry = tk.Entry(self, width=100)
-        country_entry.grid(row=0, column=1, sticky='w', pady=3)
+        country_entry = tk.Text(self, width=50, height=1)
+        country_entry.grid(row=0, column=1, sticky='w', padx=3, pady=3)
 
         province_label = tk.Label(self, text='Провинция:')
         province_label.grid(row=1, column=0, sticky='w', padx=3, pady=3)
-        province_entry = tk.Entry(self, width=100)
-        province_entry.grid(row=1, column=1, sticky='w', pady=3)
+        province_entry = tk.Text(self, width=50, height=1)
+        province_entry.grid(row=1, column=1, sticky='w', padx=3, pady=3)
 
         variety_label = tk.Label(self, text='Вид:')
         variety_label.grid(row=2, column=0, sticky='w', padx=3, pady=3)
-        variety_entry = tk.Entry(self, width=100)
-        variety_entry.grid(row=2, column=1, sticky='w', pady=3)
+        variety_entry = tk.Text(self, width=50, height=1)
+        variety_entry.grid(row=2, column=1, sticky='w', padx=3, pady=3)
 
         year_label = tk.Label(self, text='Год сбора:')
         year_label.grid(row=3, column=0, sticky='w', padx=3, pady=3)
-        year_entry = tk.Entry(self, width=100)
-        year_entry.grid(row=3, column=1, sticky='w', pady=3)
+        year_entry = tk.Entry(self, width=67)
+        year_entry.grid(row=3, column=1, sticky='w', padx=3, pady=3)
 
         points_label = tk.Label(self, text='Оценка:')
         points_label.grid(row=4, column=0, sticky='w', padx=3, pady=3)
-        points_entry = tk.Entry(self, width=100)
-        points_entry.grid(row=4, column=1, sticky='w', pady=3)
+        points_entry = tk.Entry(self, width=67)
+        points_entry.grid(row=4, column=1, sticky='w', padx=3, pady=3)
 
         price_label = tk.Label(self, text='Цена, $:')
         price_label.grid(row=5, column=0, sticky='w', padx=3, pady=3)
-        price_entry = tk.Entry(self, width=100)
-        price_entry.grid(row=5, column=1, sticky='w', pady=3)
+        price_entry = tk.Entry(self, width=67)
+        price_entry.grid(row=5, column=1, sticky='w', padx=3, pady=3)
 
         taster_label = tk.Label(self, text='Сомелье:')
         taster_label.grid(row=6, column=0, sticky='w', padx=3, pady=3)
-        taster_entry = tk.Entry(self, width=100)
-        taster_entry.grid(row=6, column=1, sticky='w', pady=3)
+        taster_entry = tk.Text(self, width=50, height=1)
+        taster_entry.grid(row=6, column=1, sticky='w', padx=3, pady=3)
 
-        taster_label = tk.Label(self, text='Описание:')
-        taster_label.grid(row=6, column=0, sticky='w', padx=3, pady=3)
-        taster_entry = tk.Entry(self, width=100)
-        taster_entry.grid(row=6, column=1, sticky='w', pady=3)
+        title_label = tk.Label(self, text='Название')
+        title_label.grid(row=7, column=0, sticky='w', padx=3, pady=3)
+        title_entry = tk.Text(self, width=50, height=1)
+        title_entry.grid(row=7, column=1, sticky='w', padx=3, pady=3)
+
+        description_label = tk.Label(self, text='Описание:')
+        description_label.grid(row=8, column=0, sticky='w', padx=3, pady=3)
+        description_entry = tk.Text(self, width=50, height=5)
+        description_entry.grid(row=8, column=1, sticky='w', padx=3, pady=3)
 
         ok_button = tk.Button(self, text='Сохранить')
-        ok_button.grid(row=7, column=0, sticky='w', padx=10, pady=3)
+        ok_button.grid(row=9, column=0, sticky='w', padx=10, pady=3)
 
         cancel_button = tk.Button(self, text='Отмена', command=lambda: self.destroy())
-        cancel_button.grid(row=7, column=1, sticky='e', padx=10, pady=3)
+        cancel_button.grid(row=9, column=1, sticky='e', padx=10, pady=3)
 
         def add_new_data():
-            country = country_entry.get()
-            province = province_entry.get()
-            variety = variety_entry.get()
+            country = country_entry.get(index1="1.0", index2="end")
+            province = province_entry.get(index1="1.0", index2="end")
+            variety = variety_entry.get(index1="1.0", index2="end")
             year = year_entry.get()
             points = points_entry.get()
             price = price_entry.get()
-            taster = taster_entry.get()
+            taster = taster_entry.get(index1="1.0", index2="end")
+            description = description_entry.get(index1="1.0", index2="end")
+            title = title_entry.get(index1="1.0", index2="end")
 
-            if re.search('\W', country) is None \
+            if re.search('[^\w\s]', country) is None \
                     and re.search('\d', country) is None \
-                    and re.search('\W', province) is None \
+                    and re.search('[^\w\s]', province) is None \
                     and re.search('\d', province) is None \
-                    and re.search('\W', variety) is None \
+                    and re.search('[^\w\s]', variety) is None \
                     and re.search('\d', variety) is None \
                     and re.search('\D', year) is None \
                     and re.search('\D', points) is None \
                     and re.search('\D', price) is None \
-                    and re.search('\W', taster) is None \
-                    and re.search('\d', taster) is None:
-                print(price)
-                self.__interactor.add_entry([country, province, variety, year, points, price, taster])
+                    and re.search('[^\w\s]', taster) is None \
+                    and re.search('\d', taster) is None\
+                    and re.search('[^\w\s]', title) is None \
+                    and re.search('\d', title) is None:
+                self.__interactor.add_entry([country[:-1], province[:-1], variety[:-1],
+                                             year, points, price, taster[:-1], description[:-1], title[:-1]])
                 self.destroy()
 
             else:
